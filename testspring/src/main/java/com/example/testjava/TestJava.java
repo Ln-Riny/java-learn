031b6904982a2a580d6cc2a905f530be8205404f
ning.li01 <ning.li01@transwarp.io>
2022-04-09 16:43:03 +0800

    初始化项目

---
+++ testspring/src/main/java/com/example/testjava/TestJava.java
@@ -0,0 +1,14 @@
+package com.example.testjava;
+
+import java.util.List;
+
+/**
+ * @author lining
+ * @date 2022/4/9
+ **/
+public class TestJava {
+    public static void main(String[] args) {
+        List<Integer>
+        System.out.println("hello world");
+    }
+}
---
+++ testspring/src/main/java/com/example/testjava/collection/TArrayList.java
@@ -0,0 +1,8 @@
+package com.example.testjava.collection;
+
+/**
+ * @author lining
+ * @date 2022/4/9
+ **/
+public class TArrayList implements  {
+}
